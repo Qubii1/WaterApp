c7c020b174242d87cfcf96d572797e8c7601fbbf
Qubii1 <quinn.bankhead@wsu.edu>
2025-05-14 13:17:33 -0700

    Created Template

---
+++ WaterApp/WaterApp/WaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterAppApp.swift
+//  WaterApp
+//
+//  Created by Quinn Bankhead on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaterApp/WaterAppTests/WaterAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaterAppTests.swift
+//  WaterAppTests
+//
+//  Created by Quinn Bankhead on 5/14/25.
+//
+
+import Testing
+@testable import WaterApp
+
+struct WaterAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
